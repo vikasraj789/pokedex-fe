bd86567b16fb62b5a31b7199d168f5718176c00d
Mac 3 <mac3@MacBook-Pro-39.local>
2018-07-30 11:11:53 +0800

    Revert "Removed infinite scroll and added pagination"
    This reverts commit 8cf0f8b02a02452226fc98245d63eecac9747e9e.
    
    Reverting local changes commit

--- src/constants.js
+++ src/constants.js
@@ -1,14 +1,14 @@
 const APP_CONSTANTS = {
-  login: "LOGIN",
-  logout: "LOGOUT",
-  fetchData: "FETCH_DATA",
-  fetchDataSuccess: "FETCH_DATA_SUCCESS",
-  fetchDataFailure: "FETCH_DATA_FAILURE",
-  putData: "PUT_DATA",
-  putDataSuccess: "PUT_DATA_SUCCESS",
-  putDataFailure: "PUT_DATA_FAILURE",
-  setMode: "SET_MODE",
-  beUrl: "https://salty-oasis-54505.herokuapp.com/"
+    login: "LOGIN",
+    logout: "LOGOUT",
+    fetchData: "FETCH_DATA",
+    fetchDataSuccess: "FETCH_DATA_SUCCESS",
+    fetchDataFailure: "FETCH_DATA_FAILURE",
+    putData: "PUT_DATA",
+    putDataSuccess: "PUT_DATA_SUCCESS",
+    putDataFailure: "PUT_DATA_FAILURE",
+    setMode: "SET_MODE",
+    beUrl: "https://salty-oasis-54505.herokuapp.com/",
 };
 
 export default APP_CONSTANTS;
--- src/containers/pokedexHolder.js
+++ src/containers/pokedexHolder.js
@@ -9,235 +9,171 @@ import PokedexHeader from "../components/pokedexHeader";
 import { getNames, getTypes } from "../utils";
 
 class PokedexHolder extends PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = {
-      items: [],
-      names: [],
-      types: [],
-      params: {
-        limit: 12,
-        offset: 0
-      },
-      paginate: 12,
-      name: "",
-      type: ""
+    constructor(props) {
+        super(props);
+        this.state = {
+            items: [],
+            names: [],
+            types: [],
+            params: {
+                limit: 20,
+                offset: 1,
+            },
+            paginate: 20,
+            name: "",
+            type: "",
+        };
+    }
+    componentDidMount() {
+        if (this.props.noFetch) {
+            const items = this.props.pokemons.list.filter(item => item.isFav);
+            const names = getNames(items);
+            const types = getTypes(items);
+            this.setState({
+                items: items,
+                names: names,
+                types: types,
+            });
+        } else {
+            this.props.fetchData({
+                url: APP_CONSTANTS.beUrl,
+                data: {
+                    userId: this.props.userId,
+                    params: this.state.params,
+                },
+            });
+        }
+    }
+    componentWillReceiveProps(nextProps) {
+        if (this.props.pokemons.list !== nextProps.pokemons.list) {
+            this.setState({
+                items: nextProps.pokemons.list,
+            });
+        }
+        if (this.props.pokemons.names !== nextProps.pokemons.names) {
+            this.setState({
+                names: nextProps.pokemons.names,
+            });
+        }
+        if (this.props.pokemons.types !== nextProps.pokemons.types) {
+            this.setState({
+                types: nextProps.pokemons.types,
+            });
+        }
+    }
+    fetchData = () => {
+        if (this.state.paginate) {
+            const state = { ...this.state };
+            const { params, paginate } = state;
+            params.offset = Math.abs(params.limit + params.offset + 1);
+            params.limit = paginate;
+            this.props.fetchData({
+                url: APP_CONSTANTS.beUrl,
+                data: {
+                    params: params,
+                },
+            });
+            this.setState({
+                params,
+            });
+        }
     };
-  }
-  componentDidMount() {
-    if (this.props.noFetch) {
-      const items = this.props.pokemons.list.filter(item => item.isFav);
-      const names = getNames(items);
-      const types = getTypes(items);
-      this.setState({
-        items: items,
-        names: names,
-        types: types
-      });
-    } else {
-      this.props.fetchData({
-        url: APP_CONSTANTS.beUrl,
-        data: {
-          userId: this.props.userId,
-          params: this.state.params
+    setPagination = limit => {
+        const paginate = Number(limit);
+        this.setState({
+            paginate: paginate ? Math.abs(paginate) : 20,
+        });
+    };
+    searchName = name => {
+        let list = this.props.pokemons.list;
+        if (this.state.type) {
+            list = list.filter(item => item.types.indexOf(this.state.type) > -1);
         }
-      });
-    }
-  }
-  componentWillReceiveProps(nextProps) {
-    if (this.props.pokemons.list !== nextProps.pokemons.list) {
-      this.setState({
-        items: nextProps.pokemons.list.slice(
-          this.state.params.offset,
-          this.state.params.limit + this.state.params.offset
-        )
-      });
-    }
-    if (this.props.pokemons.names !== nextProps.pokemons.names) {
-      this.setState({
-        names: nextProps.pokemons.names
-      });
-    }
-    if (this.props.pokemons.types !== nextProps.pokemons.types) {
-      this.setState({
-        types: nextProps.pokemons.types
-      });
-    }
-  }
-  //   fetchData = () => {
-  //     if (this.state.paginate) {
-  //       const state = { ...this.state };
-  //       const { params, paginate } = state;
-  //       params.offset = Math.abs(params.limit + params.offset);
-  //       params.limit = paginate;
-  //       this.props.fetchData({
-  //         url: APP_CONSTANTS.beUrl,
-  //         data: {
-  //           params: params
-  //         }
-  //       });
-  //       this.setState({
-  //         params
-  //       });
-  //     }
-  //   };
-  fetchData = i => {
-    const state = { ...this.state };
-    const { params, paginate } = state;
-    params.offset = i === 0 ? 0 : i * paginate;
-    params.limit = paginate;
-    const data = this.props.pokemons.list.slice(
-      params.offset,
-      params.limit + params.offset
-    );
-    if (data.length) {
-      this.setState({
-        items: data
-      });
-    } else {
-      this.props.fetchData({
-        url: APP_CONSTANTS.beUrl,
-        data: {
-          params: params
+        let data = list;
+        if (name) {
+            const items = list.filter(item => name.indexOf(item.name) > -1);
+            data = items;
         }
-      });
+        this.setState({
+            name,
+            items: data,
+        });
+    };
+    searchType = type => {
+        let list = this.props.pokemons.list;
+        let data = list;
+        if (this.state.name) {
+            list = this.state.items;
+            data = list;
+        }
+        if (type) {
+            const items = list.filter(item => item.types.indexOf(type) > -1);
+            data = items;
+        }
+        this.setState({
+            type,
+            items: data,
+        });
+    };
+    render() {
+        const { items } = this.state;
+        return (
+            <div className="pokedex-holder">
+                {this.props.pokemons.list.length > 0 && (
+                    <PokedexHeader
+                        limit={this.state.paginate}
+                        setPagination={this.setPagination}
+                        typeSuggestions={this.state.types}
+                        type={this.state.type}
+                        searchType={this.searchType}
+                        nameSuggestions={this.state.names}
+                        name={this.state.name}
+                        searchName={this.searchName}
+                    />
+                )}
+                {this.props.pokemons.list.length ? (
+                    this.state.items.length ? (
+                        <InfiniteScroll
+                            id="scroll"
+                            dataLength={items.length}
+                            next={this.fetchData}
+                            hasMore={!this.props.noFetch && !this.state.type && !this.state.name}
+                            loader={<h4 className="loading">Loading...</h4>}
+                            style={{
+                                display: "flex",
+                                flexWrap: "wrap",
+                                justifyContent: "space-around",
+                                paddingLeft: 40,
+                                paddingRight: 40,
+                                paddingTop: 100,
+                            }}
+                        >
+                            {this.state.items.map(item => {
+                                return <PokedexCard item={item} key={item.name} isProfile={this.props.isProfile} />;
+                            })}
+                        </InfiniteScroll>
+                    ) : (
+                        <div className="p-20">No pokemons found for the above filters</div>
+                    )
+                ) : (
+                    <div className="p-20">Fetching data...</div>
+                )}
+            </div>
+        );
     }
-    this.setState({
-      params
-    });
-  };
-  getPageButtons = () => {
-    const data = [];
-    for (let i = 0; i < 10; i++) {
-      const temp = (
-        <div
-          style={{
-            padding: 5,
-            marginLeft: 5,
-            marginRight: 5,
-            backgroundColor: "white",
-            cursor: "pointer"
-          }}
-          onClick={() => {
-            this.fetchData(i);
-          }}
-          key={i}
-        >
-          {i}
-        </div>
-      );
-      data.push(temp);
-    }
-    return data;
-  };
-  setPagination = limit => {
-    const paginate = Number(limit);
-    this.setState({
-      paginate: paginate ? Math.abs(paginate) : 20
-    });
-  };
-  searchName = name => {
-    let list = this.props.pokemons.list;
-    if (this.state.type) {
-      list = list.filter(item => item.types.indexOf(this.state.type) > -1);
-    }
-    let data = list;
-    if (name) {
-      const items = list.filter(item => name.indexOf(item.name) > -1);
-      data = items;
-    }
-    this.setState({
-      name,
-      items: data
-    });
-  };
-  searchType = type => {
-    let list = this.props.pokemons.list;
-    let data = list;
-    if (this.state.name) {
-      list = this.state.items;
-      data = list;
-    }
-    if (type) {
-      const items = list.filter(item => item.types.indexOf(type) > -1);
-      data = items;
-    }
-    this.setState({
-      type,
-      items: data
-    });
-  };
-  render() {
-    const { items } = this.state;
-    return (
-      <div className="pokedex-holder">
-        {this.props.pokemons.list.length > 0 && (
-          <PokedexHeader
-            limit={this.state.paginate}
-            setPagination={this.setPagination}
-            typeSuggestions={this.state.types}
-            type={this.state.type}
-            searchType={this.searchType}
-            nameSuggestions={this.state.names}
-            name={this.state.name}
-            searchName={this.searchName}
-          />
-        )}
-        {this.props.pokemons.list.length ? (
-          this.state.items.length ? (
-            <InfiniteScroll
-              id="scroll"
-              dataLength={items.length}
-              next={this.fetchData}
-              hasMore={false}
-              loader={<h4 className="loading">Loading...</h4>}
-              style={{
-                display: "flex",
-                flexWrap: "wrap",
-                justifyContent: "space-around",
-                paddingLeft: 40,
-                paddingRight: 40,
-                paddingTop: 100
-              }}
-            >
-              {this.state.items.map(item => {
-                return (
-                  <PokedexCard
-                    item={item}
-                    key={item.name}
-                    isProfile={this.props.isProfile}
-                  />
-                );
-              })}
-            </InfiniteScroll>
-          ) : (
-            <div className="p-20">No pokemons found for the above filters</div>
-          )
-        ) : (
-          <div className="p-20">Fetching data...</div>
-        )}
-        <div style={{ display: "flex", justifyContent: "center" }}>
-          {this.getPageButtons()}
-        </div>
-      </div>
-    );
-  }
 }
 
 const mapStateToProps = state => ({
-  pokemons: state.pokemons,
-  userId: state.login.user.id
+    pokemons: state.pokemons,
+    userId: state.login.user.id,
 });
 
 const mapDispatchToProps = dispatch =>
-  bindActionCreators(
-    {
-      fetchData
-    },
-    dispatch
-  );
+    bindActionCreators(
+        {
+            fetchData,
+        },
+        dispatch
+    );
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(PokedexHolder);
+export default connect(mapStateToProps, mapDispatchToProps)(PokedexHolder);
